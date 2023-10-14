722feda3c9fe74fefd06115f7aa837b6737f3cae
Octavio Martinez <omartinez1618@gmail.com>
2023-10-14 10:05:43 -0400

    whatsapp

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,6 +7,7 @@ const Home = new Component('/src/components/home/home.html',{
             username:'Zenx5',
             image:'https://avatars.githubusercontent.com/u/26119733?s=400&u=ad26266086d9fc6836ce5e180ef2f0e1a5eb68dd&v=4',
             links:[
+                { label:'WhatsApp', icon:'mdi-whatsapp', url: 'https://wa.me/584125292507' },
                 { label:'LikedIn', icon:'mdi-linkedin', url: 'https://www.linkedin.com/in/zenx5' },
                 { label:'GitHub', icon:'mdi-github', url: 'https://github.com/zenx5' },
                 {
