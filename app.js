816278b0883d443c325b6b48bb933b8cf0456b78
Octavio Martinez <omartinez1618@gmail.com>
2023-11-05 23:24:21 -0400

    cursos

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const { createVuetify } = Vuetify;
 const vuetify = createVuetify();
 
 const routeHome = await Route.create('/', Home)
+const routeCurso = await Route.create('/curso/maquetacion', CursoMaquetacion)
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes:[
-        routeHome
+        routeHome,
+        routeCurso
     ]
 })
 
