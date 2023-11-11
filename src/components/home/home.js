eb85c7fbaed1af9ceb08cd0bf818215a2837520f
Octavio Martinez <omartinez1618@gmail.com>
2023-11-11 02:35:33 -0400

    iconos coloreados

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,7 @@
 const Home = new Component('/src/components/home/home.html',{
     data() {
         return {
+            mode: true,
             langs:LangConfig.getListLang(),
             currentLang:LangConfig.getListLang()[1],
             translate:LangConfig.getLang( LangConfig.getListLang()[1] ),
@@ -8,10 +9,10 @@ const Home = new Component('/src/components/home/home.html',{
             publicRepos: 0,
             image:'https://avatars.githubusercontent.com/u/26119733?s=400&u=ad26266086d9fc6836ce5e180ef2f0e1a5eb68dd&v=4',
             links:[
-                { label:'Cursos', icon:'mdi-book', url: '#/curso/maquetacion' },
-                { label:'WhatsApp', icon:'mdi-whatsapp', url: 'https://wa.me/584125292507' },
-                { label:'LinkedIn', icon:'mdi-linkedin', url: 'https://www.linkedin.com/in/zenx5' },
-                { label:'GitHub', icon:'mdi-github', url: 'https://github.com/zenx5' },
+                { label:'Cursos', icon:'mdi-book', url: '#/curso/maquetacion', color:'#ff3333' },
+                { label:'WhatsApp', icon:'mdi-whatsapp', url: 'https://wa.me/584125292507', color:'green' },
+                { label:'LinkedIn', icon:'mdi-linkedin', url: 'https://www.linkedin.com/in/zenx5', color:'blue' },
+                { label:'GitHub', icon:'mdi-github', url: 'https://github.com/zenx5', color:'black' },
                 {
                     label:{
                         Es: "CV-Es",
@@ -23,7 +24,7 @@ const Home = new Component('/src/components/home/home.html',{
                         Es: "https://docs.google.com/presentation/d/1FwwnfO3ecUqhXlx7hAwhGJvbllphLLBun5fbn023Y0k/edit?usp=sharing",
                         En: "https://docs.google.com/presentation/d/1RKko_7pjTwIA7p5_GVu2TX-bTz71N62RTamAT904fwo/edit?usp=sharing",
                         Pt: "https://docs.google.com/presentation/d/1FwwnfO3ecUqhXlx7hAwhGJvbllphLLBun5fbn023Y0k/edit?usp=sharing"
-                    }
+                    },
                 },
             ]
         }
