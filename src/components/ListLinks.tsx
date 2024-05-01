a360818cbb8a1e5458dc35e5daf09675d817c5e4
Octavio Martinez <omartinez1618@gmail.com>
2024-05-01 11:47:55 -0400

    fix type

--- src/components/ListLinks.tsx
+++ src/components/ListLinks.tsx
@@ -19,7 +19,7 @@ export default function ListLinks({ links, currentLang }:ListLinksProps) {
     return <>
         <h2 className="text-2xl font-bold">Links</h2>
         <div className="grid md:grid-cols-4 grid-cols-2 md:grid-rows-1 grid-rows-4 gap-4">
-            {links.map( (link,index) => <ButtonLink key={index} href={getByLang(link.url)} bgcolor={link.bgcolor} color={link.color} outlined={link?.outlined}>
+            {links.map( (link,index) => <ButtonLink key={index} href={getByLang(link.url)} bgcolor={link.bgcolor} color={link.color} outlined={link?.outlined as boolean}>
                 <MdiIcon icon={link.icon} size={1} />
                 <span>{ getByLang(link.label) }</span>
             </ButtonLink> )}
