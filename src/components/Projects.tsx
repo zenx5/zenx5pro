3c35d31b102fd254638d4c8ae90cd5db95ae6922
Octavio Martinez <omartinez1618@gmail.com>
2024-05-01 22:08:42 -0400

    card de proyectos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -68,24 +68,19 @@ const items = [
 export default function Projects() {
 
 
-    return <div className="w-1/2">
+    return <div className="max-w-4xl mx-4">
         <h1 className="text-2xl font-bold text-center mb-4">Projects</h1>
         {/** Carousel of projects */}
-        <div className="w-full grid grid-cols-3 grid-flow-row gap-4 justify-between items-center">
-            {items.map( item => <div key={item.id} className="flex flex-col justify-between gap-2 border border-slate-200 rounded-lg shadow overflow-hidden w-full h-60 group">
-                <div className="flex flex-col h-44 overflow-hidden">
-                    <h2 className="mx-10 my-2 text-xl font-bold text-center">{item.title}</h2>
+        <div className="w-full grid lg:grid-cols-3 grid-cols-2 grid-flow-row gap-4 justify-between items-center">
+            {items.map( item => <div key={item.id} className="flex flex-col justify-between gap-2 border border-slate-200 rounded-lg shadow-lg overflow-hidden w-full h-72 group bg-white pt-2">
+                <div className="flex flex-col h-56 overflow-hidden">
+                    <h2 className="mx-10 my-2 text-xl font-bold text-center h-12 flex flex-col justify-center">{item.title}</h2>
                     <p className="px-4 mx-0 overflow-y-scroll" style={{ scrollbarWidth:"thin" }}>{item.description}</p>
                 </div>
-                <div className="relative h-16 border-t border-slate-200">
-                    <div className="flex flex-row gap-2 h-full w-full py-1 px-1 items-center">
-                        {item.tags.map( tag => <span key={tag} className="flex items-center justify-center text-[10px] bg-gray-200 px-2 py-1 rounded-lg">{tag}</span> )}
-                    </div>
-                    <div className="flex flex-row gap-2 absolute group-hover:top-0 items-center justify-around transition-all duration-1000 bg-white top-16 h-full w-full py-1">
+                <div className="flex flex-row gap-2 items-center justify-around border-t border-slate-200 top-16 h-16 w-full py-1">
                         <Link href={item.website ? item.website : '#'} target={item.website ? "_blank" : ""} rel="noreferrer" className={"text-sm border border-slate-200 px-4 py-1 rounded-xl" + (item.website ? " text-black cursor-pointer": " text-slate-300 cursor-default")}>Website</Link>
                         {!item.privateRepository && <Link href={item.repository ? item.repository : '#'} target={item.repository ? "_blank" : ""} rel="noreferrer" className={"text-sm border border-slate-200 px-4 py-1 rounded-xl" + (item.repository ? " text-black cursor-pointer": " text-slate-300 cursor-default")}>Repository</Link>}
                     </div>
-                </div>
             </div>)}
         </div>
     </div>
