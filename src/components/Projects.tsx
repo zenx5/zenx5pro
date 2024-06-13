fcc03ee24046910812ea0f6d1f68e3ab12fe2a0c
Octavio Martinez <omartinez1618@gmail.com>
2024-06-13 18:44:08 -0400

    width fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ import ItemProject from "./ItemProject"
 export default function Projects() {
 
 
-    return <div className="w-full grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 grid-flow-row gap-4 justify-between items-center">
+    return <div className="w-full max-w-4xl mx-4 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 grid-flow-row gap-4 justify-between items-center">
         {projects.map( item => <ItemProject key={item.id} title={item.title} description={item.description} website={item.website} repository={item.repository} privateRepository={item.privateRepository} /> )}
     </div>
 }
