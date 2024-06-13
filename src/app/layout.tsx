456e8a3e3baffbaadeef1692e288298c20acd1cf
Octavio Martinez <omartinez1618@gmail.com>
2024-06-13 18:39:54 -0400

    background relative

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Background from '@/components/Background'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,8 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className + " relative h-full w-full bg-slate-800 text-white" }>
           {children}
-          <div className="-z-10 absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]">
-          </div>
+          <Background />
       </body>
     </html>
   )
