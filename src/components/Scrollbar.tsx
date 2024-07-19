bdf3cefcd5e02a30950b34382efa8544842a3e8f
Octavio Martinez <omartinez1618@gmail.com>
2024-07-19 08:37:42 -0400

    fix

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react"
 
 export default function Scrollbar(){
     const [top, setTop] = useState(0)
-    const ref = useRef(null)
+    const ref = useRef<HTMLSpanElement>(null)
 
     function scrollController(event:any) {
         setTop( () => event.target.scrollTop )
@@ -12,7 +12,7 @@ export default function Scrollbar(){
 
     useEffect(()=>{
         if( !ref.current ) return
-        const parent = ref.current?.parentElement
+        const parent = ref.current?.parentElement as HTMLElement
         parent.addEventListener('scroll', scrollController)
 
         return ()=>parent.removeEventListener('scroll', scrollController)
@@ -21,6 +21,6 @@ export default function Scrollbar(){
 
 
     return <span ref={ref} style={{ top:`${top-1}px` }} className="absolute h-full w-4 py-5 block border-2 border-blue-500">
-        <span style={{ top:`${100*ref?.current?.scrollHeight/top}px` }} className="w-2 h-2 block bg-red-500 mx-auto"></span>
+        <span style={{ top:`${100*Number(ref?.current?.scrollHeight)/top}px` }} className="w-2 h-2 block bg-red-500 mx-auto"></span>
     </span>
 }
