7ec4b9141c8416bf3a539a6842e368a546ae1ef4
Octavio Martinez <omartinez1618@gmail.com>
2024-08-22 09:53:44 -0400

    change content

--- src/sections/MainContent.tsx
+++ src/sections/MainContent.tsx
@@ -9,14 +9,18 @@ export default function MainContent() {
             <span  className="opacity-70">Con 12 años, sin acceso a internet y en MS-DOS hice <span className="font-bold">ingeniería inversa de código en Pascal</span>, así comencé mis primeros pasos en este maravilloso mundo del desarrollo</span>
         </p>
         <p className="py-4">
-            <span  className="opacity-70">Antes de la pandemia sólo trabajé como freelance en proyectos pequeños y algunos un poco más grande como la app que hice para <span className="font-bold">IRAM-CHILE</span> la cual puedes conseguir en este <Link href="https://github.com/zenx5/iram-chile-certificados" className="hover:underline font-bold text-indigo-800">repositorio</Link></span>
+            <span  className="opacity-70">Certificado por freeCodeCamp como <Link href="https://www.freecodecamp.org/certification/zenx5/front-end-development-libraries" target="_blank" className="hover:underline text-indigo-800">Frontend Developer</Link> y <Link href="https://www.freecodecamp.org/certification/zenx5/back-end-development-and-apis" target="_blank" className="hover:underline text-indigo-800">Backend Developer</Link>, con amplia experiencia en el desarrollo de Plugins para WordPress, experto en la <Link href="https://es.react.dev/learn/thinking-in-react" className="hover:underline italic text-indigo-800">{'"'}Filosofía de React{'"'}</Link>.</span>
         </p>
         <p className="py-4">
-            <span  className="opacity-70">En 2020 decidí comenzar a trabajar con agencias, lo cual me abrió las puertas a un nuevo mundo que me ha traído grandes beneficios, entre ellos conocer gente genial de la cual he aprendido mucho</span>
+            <span  className="opacity-70"><span className="font-bold">Me he desempeñado en</span> diferentes contextos dentro de IT: maquetando interfaces, modelando base de datos, integrando modelos de IA, desarrollando API{"'"}s, optimizando y refactorizando aplicaciones, y dirigiendo equipos.</span>
         </p>
         <p className="py-4">
-            <span  className="opacity-70">Actualmente lidero un equipo de amigos con los que he desarrollado varios productos geniales como <Link href="https://chromewebstore.google.com/detail/prompt-box/odcagcgihohglhnolhimdoeahaaefilm?hl=es" className="hover:underline font-bold text-indigo-800"  >Prompt Box</Link> una extensión con aprox 150 usuarios; <Link href="https://not-json-placeholder.vercel.app/" className="hover:underline font-bold text-indigo-800">Not JSONPlaceholder</Link> un homenaje para la famosa web, que hicimos con nuestros estudiantes; y mi favorita <Link href="https://search-dependencies.vercel.app/" className="hover:underline font-bold text-indigo-800">Search Dependencies</Link> una app para búsquedas dentro de Github que seguimos desarrollando pero que ha sido una experiencia como mínimo super</span>
+            <span  className="opacity-70">
+                <span>He creado soluciones interesantes como <Link href="https://www.npmjs.com/package/kommiter-ia" className="hover:underline font-medium text-indigo-800">Kommiter IA</Link> para generar commits automaticos con Inteligencia Artificial.</span><br/>
+                <span>También implementado IA he creado <Link target="_blank" href="https://check-repo-ia.vercel.app/" className="hover:underline text-indigo-800 font-medium">Check Repo</Link>  para validar cuan bien esta hecha una App en función de como fue escrito su código.</span><br/>
+                <span>Cada una de estas soluciones las he creado motivado por mejorar la calidad de los desarrollos y la productividad de los equipos en los que he trabajado.</span>
+            </span>
         </p>
-        <Image src="/favicon.ico" alt="zenx5" width={400} height={400} className="mt-8 mx-auto w-auto h-fit"/>
+        <Image src="/favicon.ico" alt="zenx5" width={400} height={400} className="mt-2 mx-auto w-auto h-fit"/>
     </CardPing>
 }
