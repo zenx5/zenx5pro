0ac0dfc223aa6ae9fc872fe762026ab91dcbd98b
Octavio Martinez <omartinez1618@gmail.com>
2024-08-22 10:10:41 -0400

    limit articles to 6

--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -3,10 +3,10 @@ import CardPing from "@/components/CardPing"
 import { getArticlesByAuthor } from "@/services/scraping"
 
 export default async function Articles() {
-    const articles = await getArticlesByAuthor('zenx5', 8)
+    const articles = await getArticlesByAuthor('zenx5', 6)
 
     return <CardPing className="col-span-1 md:col-start-3 col-start-1" isContent h-fit>
-        {articles.slice(0,5).map( (article:any, index:number) => <Link key={index} target="_blank" href={article.href} className="py-2">
+        {articles.map( (article:any, index:number) => <Link key={index} target="_blank" href={article.href} className="py-2">
             <h2 className="text-xl opacity-70">{article.title}</h2>
             <p className="text-sm opacity-50" data-href={article.extract?.link}>{article.extract?.data}</p>
             <div className="text-xs opacity-50 flex flex-row gap-2 items-center mt-2">
